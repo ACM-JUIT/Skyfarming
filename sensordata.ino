cc2c438e0c2e5d659ed9bb73855de1e44b8025ec
Akhilesh Kumar <akhilesh_k@outlook.com>
2018-03-31 10:52:45 +0530

    Update sensordata.ino

--- sensordata.ino
+++ sensordata.ino
@@ -4,6 +4,22 @@ We need three relay switch for sprinkler, pump and fan
 sprinkler depends on soil moisture level
 pump on water level
 and fan on temprature
+
+conditions: 
+1. if: Water level is less than any x value, HIGH the corresponding relay pin for water pump.
+else: LOW
+
+2. if: Temprture is more than any y value, HIGH the corresponding relay pin for fan.
+else: LOW
+
+3. if: Soil Moisture level is less than any value z, HIGH the relay pin for sprinkler.
+else: LOW
+
+
+connections:
+dht11 to A0
+soil moisture to A1
+Water level to A2
 */
 
 
@@ -20,20 +36,28 @@ void setup(){
 }
 void loop(){
   DHT.read11(dht_apin);
+  delay(10);
   soilout=analogRead(soilmoisture);
   soilout=map(soilout,550,10,0,100);
   Serial.print("Moisture : ");
   Serial.print(soilout);
   Serial.println("%");
-  Serial.print("Current Humidity=");
+  Serial.print("Current Humidity=");   //remove this, we don;t need to print class for the values
   Serial.print(DHT.humidity);
-  Serial.print("%\t");
-  Serial.print("Current Temperature");
+  Serial.print(",");            //we will use comma to sperate in python string
+  Serial.print("Current Temperature"); //remove this, we don;t need to print class for the values
   Serial.print(DHT.temperature);
   Serial.println("C");
   waterlevel=analogRead(water);
-  Serial.println(waterlevel);
+  Serial.println(waterlevel); 
   delay(5000);
+  /*
+  do all the logic stuff and check other left things
+  - relay
+  - values in string
+  */
+  
+  
   if (soilout<100){
   pinMode(sprinkle,HIGH);
   else{
